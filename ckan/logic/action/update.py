cc48182f7fd8ed02c9f832ed30ec34d7bd719995
David Read <david.read@hackneyworkshop.com>
2012-10-31 10:14:33 +0000

    Task status updates now trigger search index update.

--- ckan/logic/action/update.py
+++ ckan/logic/action/update.py
@@ -13,6 +13,7 @@ import ckan.lib.dictization.model_save as model_save
 import ckan.lib.navl.dictization_functions
 import ckan.lib.navl.validators as validators
 import ckan.lib.plugins as lib_plugins
+import ckan.model.domain_object
 
 log = logging.getLogger(__name__)
 
@@ -481,6 +482,24 @@ def task_status_update(context, data_dict):
 
     session.commit()
     session.close()
+
+    # Task status is now used to store more transient info about the package
+    # and so things like search indexing now depend on task status (e.g. 5 stars).
+    # Therefore send out a notification that the domain object has changed.
+    entity_id = data_dict.get('entity_id')
+    if entity_id:
+        dome = model.modification.DomainObjectModificationExtension()
+        try:
+            domain_object = logic.action.get_domain_object(model, entity_id)
+        except NotFound:
+            pass
+        else:
+            if isinstance(domain_object, model.ResourceRevision):
+                if domain_object.resource_group:
+                    if domain_object.resource_group.package:
+                        dome.notify(domain_object.resource_group.package,
+                                    ckan.model.domain_object.DomainObjectOperation.changed)
+
     return model_dictize.task_status_dictize(task_status, context)
 
 def task_status_update_many(context, data_dict):
